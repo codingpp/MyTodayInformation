85b95c9e586d5a07213e4097c90e386675075ed2
sunpan <sunpan920@gmail.com>
2020-08-06 17:26:17 +0800

    启动页面完善

---
+++ app/src/main/java/com/coding/mytodayinformation/base/BaseActivity.kt
@@ -0,0 +1,16 @@
+package com.coding.mytodayinformation.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *
+ * @author SunPan
+ * @date 2020/8/6
+ */
+open class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
--- app/src/main/java/com/coding/mytodayinformation/ui/SplashActivity.java
+++ app/src/main/java/com/coding/mytodayinformation/ui/SplashActivity.java
@@ -1,15 +1,28 @@
 package com.coding.mytodayinformation.ui;
 
-import android.media.MediaPlayer;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
 import com.coding.mytodayinformation.R;
+import com.coding.mytodayinformation.base.BaseActivity;
 import com.coding.mytodayinformation.view.FullScreenVideoView;
 
 import java.io.File;
+import java.lang.ref.WeakReference;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 /**
  * 启动页
@@ -17,28 +30,84 @@ import java.io.File;
  * @author codingPan
  * @date 2019-10-30
  */
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BaseActivity {
+
+    /**
+     * 视频总时长，精确到秒
+     */
+    private int videoDuration;
+
+    @BindView(R.id.vvPlay)
+    FullScreenVideoView vvPlay;
+    @BindView(R.id.tvTimer)
+    TextView tvTimer;
+
+    private MyHandler myHandler = new MyHandler(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
 
-        FullScreenVideoView vvPlay = findViewById(R.id.vvPlay);
         vvPlay.setVideoURI(Uri.parse("android.resource://" + getPackageName() + File.separator + R.raw.splash));
-        vvPlay.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                //播放器准备完毕后开始播放
-                mediaPlayer.start();
-            }
+        vvPlay.setOnPreparedListener(mediaPlayer -> {
+            //播放器准备完毕后开始播放
+            mediaPlayer.start();
+            videoDuration = mediaPlayer.getDuration() / 1000;
+            String timerText = getString(R.string.jump) + videoDuration;
+            tvTimer.setText(timerText);
+            startTimer();
         });
-        vvPlay.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                //播放完毕后重新开始播放
-                mediaPlayer.start();
-            }
-        });
+        vvPlay.setOnCompletionListener(mediaPlayer -> jumpToMain());
+    }
+
+    /**
+     * 启动倒计时
+     */
+    private void startTimer() {
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        Runnable runnable = () -> myHandler.sendEmptyMessage(0);
+        service.scheduleAtFixedRate(runnable, 1, 1, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 跳转到主页
+     */
+    private void jumpToMain() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * 设置倒计时文字
+     */
+    private void setTvTimerText() {
+        videoDuration--;
+        int count = Math.max(videoDuration, 0);
+        String timerText = getString(R.string.jump) + count;
+        tvTimer.setText(timerText);
+    }
+
+    @OnClick(R.id.tvTimer)
+    public void onViewClicked() {
+        jumpToMain();
+    }
+
+    private static class MyHandler extends Handler {
+
+        private final WeakReference<SplashActivity> mReference;
+
+        private MyHandler(SplashActivity activity) {
+            mReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            SplashActivity activity = mReference.get();
+            activity.setTvTimerText();
+        }
     }
 }
